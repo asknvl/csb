902b266a995c4e894f60f5d2defee95605c66dc9
asknvl <asknvl@gmail.com>
2022-07-21 14:18:08 +0300

    wip

---
+++ csb/matching/TextMatchingAnalyzer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csb.matching
+{
+    public class TextMatchingAnalyzer : ITextMatchingAnalyzer
+    {
+
+        #region const
+        private const int store_time = 1440; 
+        #endregion
+
+        #region vars
+        Queue<string[]> messageQueue;
+        #endregion
+
+        #region properties
+        #endregion
+
+        public TextMatchingAnalyzer(int period)
+        {
+
+        }
+
+        #region public
+        public void Add(string message)
+        {
+            var splt = message.Split(" ");
+            //messageList.Add(splt);
+        }
+
+        public bool Check(string message)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
