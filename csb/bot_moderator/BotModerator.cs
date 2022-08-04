ab86a10b4499e7dc2fab51fd2148df40a66b0d14
asknvl <asknvl@gmail.com>
2022-08-04 17:53:17 +0300

    wip

--- csb/bot_moderator/BotModerator.cs
+++ csb/bot_moderator/BotModerator.cs
@@ -38,6 +38,10 @@ namespace csb.bot_moderator
         public string Token { get; set; }
         [JsonIgnore]
         public bool IsRunning { get; set; }
+        [JsonIgnore]
+        public uint RequestsCounter { get; set; }
+        public uint ApprovesCounter { get; set; }
+        public uint ApisendsCounter { get; set; }
         #endregion
 
         public BotModerator()
@@ -60,10 +64,15 @@ namespace csb.bot_moderator
             {
                 try
                 {
+                    RequestsCounter++;
+
                     var chatJoinRequest = update.ChatJoinRequest;                    
-                    Console.WriteLine($"join request: {Name} from {chatJoinRequest.From.FirstName} {chatJoinRequest.From.LastName}");
+                    //Console.WriteLine($"join request: {Name} from {chatJoinRequest.From.FirstName} {chatJoinRequest.From.LastName}");
                     bool res = await bot.ApproveChatJoinRequest(chatJoinRequest.Chat.Id, chatJoinRequest.From.Id);
-                    Console.WriteLine("Result=" + res);
+                    //Console.WriteLine("Result=" + res);
+
+                    if (res)
+                        ApprovesCounter++;
 
                     if (res)
                     {
@@ -80,12 +89,16 @@ namespace csb.bot_moderator
 
                         });
                         await statApi.AddFollowers(followers);
+                        ApisendsCounter++;
                     }
 
+                    string info = $"{DateTime.Now} {Name}: req {RequestsCounter.ToString().PadLeft(6)} from {chatJoinRequest.From.Username} approved={res} approves={ApprovesCounter.ToString().PadLeft(6)} apicntr={ApisendsCounter.ToString().PadLeft(6)}";
+                    Console.WriteLine(info);
 
-                } catch (Exception ex)
+                }
+                catch (Exception ex)
                 {
-                    Console.WriteLine(ex.Message);
+                    Console.WriteLine($"------------------------ {DateTime.Now} {ex.Message} --------------------------------");
                 }
             }
         }
