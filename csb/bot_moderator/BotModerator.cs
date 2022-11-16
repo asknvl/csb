dcdbe6743ed8a828993723267d62d24285e18d28
aslnvl <asknvl@gmail.com>
2022-11-16 10:17:54 +0300

    wip

--- csb/bot_moderator/BotModerator.cs
+++ csb/bot_moderator/BotModerator.cs
@@ -21,8 +21,8 @@ namespace csb.bot_moderator
         protected CancellationTokenSource cts;
 
 #if DEBUG
-        //protected TGStatApi statApi = new TGStatApi("http://185.46.9.229:4000");
-        protected TGStatApi statApi = new TGStatApi("http://136.243.74.153:4000");
+        protected TGStatApi statApi = new TGStatApi("http://185.46.9.229:4000");
+        //protected TGStatApi statApi = new TGStatApi("http://136.243.74.153:4000");
 
 #else
         //TGStatApi statApi = new TGStatApi("http://136.243.74.153:4000");
