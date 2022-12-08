b37134bd0a0ce0e16f42c27cc3a9734c66c33d86
asknvl <asknvl@gmail.com>
2022-12-08 17:48:40 +0300

    wip

--- csb/Program.cs
+++ csb/Program.cs
@@ -18,7 +18,7 @@ namespace csb
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Вдудь 3.1.9");
+            Console.WriteLine("Вдудь 3.1.9_1");
             BotManager manager = new BotManager();
             manager.Start();
 
